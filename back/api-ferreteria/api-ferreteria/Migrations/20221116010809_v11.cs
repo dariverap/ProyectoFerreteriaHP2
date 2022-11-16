cd2f853a015f83f8976e7d3e44870f9315948d96
dariverap <08diegorivera@gmail.com>
2022-11-15 20:29:00 -0500

    arreglo3

---
+++ back/api-ferreteria/api-ferreteria/Migrations/20221116010809_v11.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_ferreteria.Migrations
+{
+    public partial class v11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "descripcion",
+                table: "Categoria",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ front/api-ferreteria-ts/src/Clientes/cliente.model.d.ts
@@ -0,0 +1,8 @@
+export interface ClienteDTO{
+    codigo: integer;
+    nombre:string;
+    apellido: string;
+    numdocumento: string;
+    estado: boolean;
+
+}
